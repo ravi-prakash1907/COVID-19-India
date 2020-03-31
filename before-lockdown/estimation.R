2fdea1b0986641b0bb08339e8ecc85cb11d46f9d
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-03-31 16:31:48 +0530

    initializing primary R script of the analysis

---
+++ before-lockdown/estimation.R
@@ -0,0 +1,8 @@
+##
+## incubatio period -> in india ---> assume 10-12
+incubation.Period = 10
+
+# Time from symptom onset to death
+incub.toDeath = 15
+
+## total time taken from incubation to death --->  10+15 = 25
